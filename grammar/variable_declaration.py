6c9b140d5a4c5d6a62c0d297937e3e87157ccff1
SinaVafadar <snvfdr@gmail.com>
2020-05-15 02:46:25 +0430

    Variable Decalaration Updated - List Recognition Added

--- grammar/variable_declaration.py
+++ grammar/variable_declaration.py
@@ -1,4 +1,5 @@
 import functools
+
 import inflect
 
 
@@ -31,6 +32,47 @@ class VariableDeclaration:
         value = str(functools.reduce(lambda first, second: f'{first} {second}', input_string))
         return value
 
+    def find_last_index_of_string(self, input_list: list) -> int:
+        for index, item in enumerate(input_list):
+            if item == 'next' and input_list[index + 1] in self.type:
+                return index
+            if item == 'end' and input_list[index + 1] == 'of' and input_list[index + 2] == 'list':
+                return index
+
+    @staticmethod
+    def find_last_index_of_list(input_list: list) -> int:
+        for index, item in enumerate(input_list):
+            if item == 'end' and input_list[index + 1] == 'of' and input_list[index + 2] == 'list':
+                return index + 3
+
+    def parse_list(self, input_list: list) -> list:
+        this_type, this_list = None, []
+        index, item = -1, None
+        while True:
+            index += 1
+            item = input_list[index]
+            if item == 'end' and index + 3 == len(input_list):
+                break
+            if item == 'next':
+                continue
+            if item in self.variable_types and (input_list[index - 1] != 'list' or index == 0):
+                this_type = item
+                continue
+            if this_type == 'integer':
+                this_list.append(int(item))
+            elif this_type == 'float':
+                this_list.append(float(f'{item}.{input_list[index + 2]}'))
+                index += 2
+            elif this_type == 'string':
+                end_of_string = self.find_last_index_of_string(input_list[index:]) + index
+                this_list.append(self.parse_string(input_list[index:end_of_string]))
+                index += end_of_string - index - 1
+            elif this_type == 'list':
+                end_of_list = self.find_last_index_of_list(input_list[index:]) + index
+                this_list.append(self.parse_list(input_list[index:end_of_list]))
+                index += end_of_list - index - 1
+        return this_list
+
     def find_value(self) -> str:
         is_keyword_index = self.find_is_keyword_index()
         value = None
@@ -40,6 +82,8 @@ class VariableDeclaration:
             value = f'{self.command[is_keyword_index + 2]}.{self.command[is_keyword_index + 4]}'
         elif self.type == 'string':
             value = self.parse_string(self.command[is_keyword_index + 2:])
+        elif self.type == 'list':
+            value = self.parse_list(self.command[is_keyword_index + 2:])
         return value
 
     def find_name(self) -> str:
