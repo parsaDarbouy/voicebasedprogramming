0150470a1f8ffd571787fad6443abb40fa6c9a7b
SinaVafadar <snvfdr@gmail.com>
2020-06-05 00:30:56 +0430

    Right Keyword Recognition - Bug Fixed

--- right_keyword_interpretation/right_keyword.py
+++ right_keyword_interpretation/right_keyword.py
@@ -1,14 +1,15 @@
 import fuzzy
+from libindic.soundex import Soundex
 from difflib import SequenceMatcher
 
 keyword_list = [('def', 0.5, 'D1'), ('is', 0.5, 'I2'), ('variable', 0.5, 'V614'), ('string', 0.5, 'S365'),
                 ('integer', 0.5, 'I532'), ('float', 0.5, 'F432'), ('list', 0.5, 'L232'), ('Dictionary', 0.5, 'D235')]
 
-soundex = fuzzy.Soundex(4)
+soundex = Soundex()
 
 
 def right_keyword(word, keywords):
-    sound = soundex(word)
+    sound = soundex.soundex(word)
     for i in keywords:
         key_word = i[0]
         key_word_sound = i[2]
