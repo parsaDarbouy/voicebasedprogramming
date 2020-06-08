0e54b349d6362c4e9c33cb1a21295cf315cde51a
sepehr javid <javid.sepehr77@gmail.com>
2020-06-08 18:37:34 +0430

    refactor voice recognition

--- voice_recognition/recognizers.py
+++ voice_recognition/recognizers.py
@@ -3,14 +3,12 @@ import speech_recognition
 
 class GoogleRecognizer(object):
     def __init__(self, noise_adjust_duration=5):
-        self.recognizer = speech_recognition.Recognizer()
         self.noise_adjust_duration = noise_adjust_duration
         self.current_result_text = ""
 
     def recognize(self):
         with speech_recognition.Microphone() as source:
             self.recognizer.adjust_for_ambient_noise(source, duration=self.noise_adjust_duration)
-            print('fuck u')
             audio = self.recognizer.listen(source)
         try:
             self.current_result_text = self.recognizer.recognize_google(audio)
