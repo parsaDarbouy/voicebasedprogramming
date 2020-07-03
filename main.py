03be1f996810a58199fe70771d9d161366829af0
sepehr javid <javid.sepehr77@gmail.com>
2020-07-03 17:38:14 +0430

    add remove line section in main

--- main.py
+++ main.py
@@ -49,12 +49,23 @@ class Root(Tk):
             text = self.recognizer.recognize()
             words_list = keyword_recognition(text)
             print(text)
+            if words_list[0] == "remove" and words_list[1] == "line":
+                line_number = int(words_list[2])
+                temp_file = open(self.filename, "w")
+                lines = temp_file.readlines()
+                lines.remove(lines[line_number - 1])
+                file.close()
+                temp_file.close()
+                temp_file = open(self.filename, "w")
+                temp_file.writelines(lines)
+                temp_file.close()
             self.code_converter.set_command(words_list)
             if ' '.join(words_list[-3:]) in self.revert_indent_phrases and self.indent - 1 >= 0:
                 self.indent -= 1
             file.write((self.indent * '\t') + self.code_converter.generate_code() + "\n")
             if ' '.join(words_list[:2]) in self.indent_phrases:
                 self.indent += 1
+            file.close()
 
 
 root = Root()
