fa849d56195313c8d2108522c8b7bf2ce0636aee
unknown <rahil.farzanegan@gmail.com>
2020-07-24 16:30:40 +0430

    else and elif test completed

--- grammar/test_command_to_code.py
+++ grammar/test_command_to_code.py
@@ -5,6 +5,7 @@ from variable_declaration import VariableDeclaration
 from return_function import ReturnFunction
 from if_condition import IfCondition
 from function_definition import FunctionDefinition
+from else_condition import ElseCondition
 
 class TestCommandToCode(unittest.TestCase):
 
@@ -31,6 +32,24 @@ class TestCommandToCode(unittest.TestCase):
         self.assertEqual(
             IfCondition('if condition float 5 point 98 is less than variable my third number'.split()).code,
             'if 5.98 < my_third_number:')
+
+    def test_elif_condition(self):
+        self.assertEqual(
+            IfCondition('else if condition variable my first var is equal to string rahil'.split()).code,
+            'elif my_first_var == \'rahil\':')
+        self.assertEqual(
+            IfCondition('else if condition integer 2 is not equal to variable my first number'.split()).code,
+            'elif 2 != my_first_number:')
+        self.assertEqual(
+            IfCondition('else if condition variable my second var is greater than or equal to variable my second number'.split()).code,
+            'elif my_second_var >= my_second_number:')
+        self.assertEqual(
+            IfCondition('else if condition float 5 point 98 is less than variable my third number'.split()).code,
+            'elif 5.98 < my_third_number:')
+
+    def test_if_condition(self):
+        self.assertEqual(
+            ElseCondition('else condition'.split()).code, 'else:')
 
     def test_return_function(self):
         self.assertEqual(
